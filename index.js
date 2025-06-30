38a0be70174411b55ca244c91a5c5d744adbe927
Nelson Garcia <nelsongarciapjr@gmail.com>
2025-06-30 14:24:51 -0300

    Mensagens de contribuição de outro DEV do sistema atualizadas!

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ import { turboLogger } from "./turboLogger.js";
 
 console.log("Teste de uso do Github");
 turboLogger("Estou aprendendo o fluxo do git", "red");
+turboLogger(
+  "Contribuição de outro dev na branch principal antes do merge",
+  "orange"
+);
